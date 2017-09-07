e3637d1ad06a2cf176b6f38d64f1d12e65e1e94e
itsMontoya <josh@montoya.io>
2017-09-07 11:16:15 -0700

    Updating helloWorld example to show multiple boxes

--- examples/helloWorld/main.go
+++ examples/helloWorld/main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"image/color"
+	"os"
+
 	"github.com/itsmontoya/blueprint"
 	"github.com/missionMeteora/journaler"
 )
 
+var (
+	blackC = color.RGBA{0, 0, 0, 255}
+	whiteC = color.RGBA{255, 255, 255, 255}
+)
+
 func main() {
 	var (
-		//b   *blueprint.Blueprint
-		err error
+		b = blueprint.New("Test app", blueprint.Rects{Width: 640, Height: 480}, blueprint.NilColor)
+		r = blueprint.Rects{Width: 300, Height: 250}
+
+		c *blueprint.Container
+
+		s1, s2 blueprint.Style
+		err    error
 	)
 
-	if _, err = blueprint.New("Test app", 640, 480, blueprint.NilColor); err != nil {
-		journaler.Error("Error initializing blueprint: %v", err)
+	if s1, err = blueprint.NewStyle(r, blueprint.Padding{}, blueprint.Margin{}, blackC, whiteC); err != nil {
+		journaler.Error("Error getting new style: %v", err)
+		os.Exit(1)
 	}
 
-	//b.Run()
+	if s2, err = blueprint.NewStyle(r, blueprint.Padding{}, blueprint.Margin{}, whiteC, blackC); err != nil {
+		journaler.Error("Error getting new style: %v", err)
+		os.Exit(1)
+	}
+
+	c = blueprint.NewContainer(s1, blueprint.Coords{X: 12, Y: 12})
+	b.Push(c)
+
+	c = blueprint.NewContainer(s2, blueprint.Coords{X: 24, Y: 24})
+	b.Push(c)
+
+	c = blueprint.NewContainer(s1, blueprint.Coords{X: 36, Y: 36})
+	b.Push(c)
+
+	c = blueprint.NewContainer(s2, blueprint.Coords{X: 48, Y: 48})
+	b.Push(c)
+
+	if err = b.Run(); err != nil {
+		journaler.Error("Error initializing blueprint: %v", err)
+		os.Exit(1)
+	}
 }
